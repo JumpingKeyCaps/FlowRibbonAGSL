42a7605aabda30195962e52e184f2559c3e80681
Samuel Lebailly <sam.meusa@gmail.com>
2025-12-01 23:24:02 +0100

    feature(main/): Clean and call the composition to test the rendering.

--- app/src/main/java/com/lebaillyapp/flowribbonagsl/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/flowribbonagsl/MainActivity.kt
@@ -10,7 +10,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.lebaillyapp.flowribbonagsl.flowRibbon.FlowRibbonDebug
+import com.lebaillyapp.flowribbonagsl.flowRibbon.FlowRibbonShader
 import com.lebaillyapp.flowribbonagsl.ui.theme.FlowRibbonAGSLTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FlowRibbonAGSLTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                //debug
+               // FlowRibbonDebug()
+
+                // real one !
+            //    /**
+
+                FlowRibbonShader(
+                modifier = Modifier.fillMaxSize()
+                .scale(1.0f)
+                .alpha(1.0f),
+                color = Color.hsv(71.31f, 1f, 1f),
+                distortionStrength = 3.18f,
+                highlightIntensity = 0.33f,
+                gamma = 1.86f,
+                horizontalMix = 0.72f,
+                microStrength = 0f,
+                autoAnimate = true,
+                scaleOverride = 1.85f,
+                rotationOverride = 0f,
+                timeSpeedOverride = 1.24f
+                )
+            //     */
+
+
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlowRibbonAGSLTheme {
-        Greeting("Android")
     }
 }
